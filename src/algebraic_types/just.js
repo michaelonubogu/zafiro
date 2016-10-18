aab483c0a5a3dbfc64fd2b019eb1d052eba666c8
remojansen <remo.jansen@wolksoftware.com>
2016-10-18 21:22:24 +0100

    remove js

--- src/algebraic_types/just.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Just = (function () {
-    function Just(val) {
-        this.value = val;
-    }
-    return Just;
-}());
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Just;
